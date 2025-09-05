4f32877e0a5f7d07980c3edf8fa3d5754e143caa
stevens509 <155673843+stevens509@users.noreply.github.com>
2025-09-04 20:23:43 -0400

    Update nfs-storageclass.sh

--- nfs-storageclass.sh
+++ nfs-storageclass.sh
@@ -1,3 +1,3 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
-helm install -n nfs-provisioning --create-namespace nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=192.168.170.131 --set nfs.path=/opt/dynamic-storage
+helm install -n nfs-provisioning --create-namespace nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=192.168.52.132 --set nfs.path=/opt/dynamic-storage
 kubectl patch storageclass nfs-client -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
